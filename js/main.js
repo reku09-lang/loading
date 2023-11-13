a5f9ae8fc7bb1ef50fbef57a8590519baa673f44
reku09-lang <qxhke76953@yahoo.co.jp>
2023-11-14 00:38:37 +0900

    prettier適用

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-$(function(){
-    $(window).on('load', function(){
-        $('.pin').css({
-            transform: "rotate(140deg)",
-        });
-        $('.loading').delay(2000).fadeOut(300);
+$(function () {
+  $(window).on("load", function () {
+    $(".pin").css({
+      transform: "rotate(140deg)",
     });
+    $(".loading").delay(2000).fadeOut(300);
+  });
 });
